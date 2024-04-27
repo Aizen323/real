336d5084cc94f501c348d6143167357e73d09ff6
sdac-10 <sdac-10@sdac-10-PC>
2024-04-27 03:11:40 -0700

    4

--- real/src/real/Main.java
+++ real/src/real/Main.java
@@ -5,6 +5,7 @@ public static void main(String[]args){
 	System.out.println("Konnichiwa");
 	System.out.println("Sosuke Aizen");
 	System.out.println("Yokoso watashi no soul society");
+	System.out.println("Squad 5");
 	
 }
 	
